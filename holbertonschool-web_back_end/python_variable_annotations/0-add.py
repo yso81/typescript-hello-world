612a26718d2cb8f98e4fce434a3aab14d8f98f75
Yannick <yannicksookree@gmail.com>
2026-01-05 14:44:14 +0400

    upd

--- holbertonschool-web_back_end/python_variable_annotations/0-add.py
+++ holbertonschool-web_back_end/python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to add two numbers.
+"""
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+    """
     return a + b
