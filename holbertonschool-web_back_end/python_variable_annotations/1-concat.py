39b5a2b16908d13ef011c1c311fc866b68b01bf5
Yannick <yannicksookree@gmail.com>
2026-01-06 10:03:29 +0400

    upd

---
+++ holbertonschool-web_back_end/python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Function that takes 2 strings as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that concatenates str1 and str2
+
+    Args:
+    str1: str
+    str2: str
+
+    Return:
+    str: str1 + str2
+    """
+    return str1 + str2
